3b7b3ef475ce7281432159d8b173f0bf9ea947b3
Ayush2000Roy <67708014+Ayush2000Roy@users.noreply.github.com>
2021-03-31 19:25:59 +0530

    Final State of App

---
+++ lib/models/transaction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+
+// Blueprint of the object trans.
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}
